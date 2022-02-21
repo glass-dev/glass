aad91028e47efc13764449e43f5acaeea2d60e40
Nicolas Tessore <n.tessore@ucl.ac.uk>
2022-02-21 11:49:03 +0000

    fix import warnings

--- glass/__init__.py
+++ glass/__init__.py
@@ -6,6 +6,6 @@ __version__ = '2022.2.18'
 
 
 # utility imports from internal modules
-from ._generator import generator
-from ._simulate import zspace, xspace, lightcone
-from ._user import logger
+from ._generator import generator  # noqa: F401
+from ._simulate import zspace, xspace, lightcone  # noqa: F401
+from ._user import logger  # noqa: F401
--- glass/_user.py
+++ glass/_user.py
@@ -4,7 +4,6 @@
 
 import logging
 from contextlib import contextmanager
-import numpy as np
 
 
 @contextmanager
