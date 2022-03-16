f138dee2278c6c9dd5d1dc2d7bb7b2fceb15c761
Arthur Loureiro <arthur.loureiro@ucl.ac.uk>
2022-03-16 14:48:14 +0000

    ENH: apply tomographic binning to redshift distribution (#9)
    Co-authored-by: Nicolas Tessore <n.tessore@ucl.ac.uk>

--- glass/observations.py
+++ glass/observations.py
@@ -36,8 +36,10 @@ Visibility
 import logging
 import numpy as np
 import healpy as hp
+import math
 
 from .core import generator
+from .util import cumtrapz
 
 
 log = logging.getLogger(__name__)
@@ -119,3 +121,125 @@ def vmap_galactic_ecliptic(nside, galactic=(30, 90), ecliptic=(20, 80)):
     m = hp.Rotator(coord='CE').rotate_map_pixel(m)
 
     return m
+
+
+def fixed_zbins(zmin, zmax, *, nbins=None, dz=None):
+    '''tomographic redshift bins of fixed size
+
+    This function creates contiguous tomographic redshift bins of fixed size.
+    It takes either the number or size of the bins.
+
+    Parameters
+    ----------
+    zmin, zmax : float
+        Extent of the redshift binning.
+    nbins : int, optional
+        Number of redshift bins.  Only one of ``nbins`` and ``dz`` can be given.
+    dz : float, optional
+        Size of redshift bin.  Only one of ``nbins`` and ``dz`` can be given.
+
+    Returns
+    -------
+    zbins : list of tuple of float
+        List of redshift bin edges.
+    '''
+
+    if (nbins is None) == (dz is None):
+        raise ValueError('either nbins or dz must be given')
+
+    if nbins is not None:
+        zbinedges = np.linspace(zmin, zmax, nbins+1)
+    if dz is not None:
+        zbinedges = np.arange(zmin, zmax, dz)
+
+    return list(zip(zbinedges, zbinedges[1:]))
+
+
+def equal_dens_zbins(z, nz, nbins):
+    '''equal density tomographic redshift bins
+
+    This function subdivides a source redshift distribution into ``nbins``
+    tomographic redshift bins with equal density.
+
+    Parameters
+    ----------
+    z, nz : array_like
+        The source redshift distribution. Must be one-dimensional.
+    nbins : int
+        Number of redshift bins.
+
+    Returns
+    -------
+    zbins : list of tuple of float
+        List of redshift bin edges.
+
+    '''
+    # compute the normalised cumulative distribution function
+    # first compute the cumulative integral (by trapezoidal rule)
+    # then normalise: the first z is at CDF = 0, the last z at CDF = 1
+    # interpolate to find the z values at CDF = i/nbins for i = 0, ..., nbins
+    cuml_nz = cumtrapz(nz, z)
+    cuml_nz /= cuml_nz[[-1]]
+    zbinedges = np.interp(np.linspace(0, 1, nbins+1), cuml_nz, z)
+
+    return list(zip(zbinedges, zbinedges[1:]))
+
+
+def tomo_nz_gausserr(z, nz, sigma_0, zbins):
+    '''tomographic redshift bins with a Gaussian redshift error
+
+    This function takes a _true_ overall source redshift distribution ``z``,
+    ``nz`` and returns tomographic source redshift distributions for the
+    tomographic redshift bins given by ``zbins``.  It is assumed that sources
+    are assigned a tomographic redshift bin with a Gaussian error [1]_. The
+    standard deviation of the Gaussian depends on redshift and is given by
+    ``sigma(z) = sigma_0*(1 + z)``.
+
+    Parameters
+    ----------
+    z, nz : array_like
+        The true source redshift distribution. Must be one-dimensional.
+    sigma_0 : float
+        Redshift error in the tomographic binning at zero redshift.
+    zbins : list of tuple of float
+        List of redshift bin edges.
+
+    Returns
+    -------
+    binned_nz : array_like
+        Tomographic redshift bins convolved with a gaussian error.
+        Array has a shape (nbins, len(z))
+
+    See Also
+    --------
+    equal_dens_zbins :
+        produce equal density redshift bins
+    fixed_zbins :
+        produce redshift bins of fixed size
+
+    References
+    ----------
+    .. [1] Amara A., Réfrégier A., 2007, MNRAS, 381, 1018.
+           doi:10.1111/j.1365-2966.2007.12271.x
+
+    '''
+    # converting zbins into an array:
+    zbins = np.asanyarray(zbins)
+
+    # bin edges and adds a new axis
+    z_lower = zbins[:, 0, np.newaxis]
+    z_upper = zbins[:, 1, np.newaxis]
+
+    # we need a vectorised version of the error function:
+    erf = np.vectorize(math.erf, otypes=(float,))
+
+    # compute the probabilities that redshifts z end up in each bin
+    # then apply probability as weights to given nz
+    # leading axis corresponds to the different bins
+    sz = 2**0.5*sigma_0*(1 + z)
+    binned_nz = erf((z - z_lower)/sz)
+    binned_nz -= erf((z - z_upper)/sz)
+    binned_nz /= 1 + erf(z/sz)
+    binned_nz *= nz
+
+    return binned_nz
