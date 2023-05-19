6aae4a2737ad73f4f6f1e09afc11aa272a38c8a5
Nicolas Tessore <n.tessore@ucl.ac.uk>
2023-05-20 00:46:34 +0100

    API(galaxies): change handling of array inputs and populations (#83)
    This change brings the inputs of the `glass.galaxies` module in line
    with the outputs of the `glass.points` sampling functions after changes
    #80 and #86.
    
    The `redshifts_from_nz()` function now supports arrays for the `count`
    parameter (e.g. as returned by the `glass.points` functions) as well as
    multi-dimensional distributions. If multiple populations are being
    sampled, the resulting output from each population (i.e. extra input
    dimension) is concatenated and returned as a flat 1-D column.
    
    The old `gal_pop` return value no longer exists; the population
    information is already decided by the points sampling and encoded in the
    `count` array. As mentioned in #85, it is now easy to generate a column
    of arbitrary population labels using `np.repeat(labels, count.flat)`.
    
    Also changed is that `gaussian_phz()` accepts array input for the
    `sigma_0` parameter. This can be used to sample photometric redshifts
    with different uncertainties for different populations, using
    `np.repeat(sigma_0_for_each_pop, count.flat)` to create a column of
    `sigma_0` values for all sampled galaxies.
    
    BREAKING CHANGE: `redshifts_from_nz()` no longer returns `gal_pop`
    
    Fixes: #82

--- glass/galaxies.py
+++ glass/galaxies.py
@@ -18,37 +18,45 @@ Functions
 
 '''
 
+from __future__ import annotations
+
 import numpy as np
 import healpix
 
-from typing import Optional, Tuple
+from numpy.typing import ArrayLike
 
-from .math import cumtrapz
+from .math import broadcast_leading_axes, cumtrapz
 
 
-def redshifts_from_nz(size: int, z: np.ndarray, nz: np.ndarray, *,
-                      rng: Optional[np.random.Generator] = None
-                      ) -> Tuple[np.ndarray, Optional[np.ndarray]]:
+def redshifts_from_nz(count: int | ArrayLike, z: ArrayLike, nz: ArrayLike, *,
+                      rng: np.random.Generator | None = None
+                      ) -> np.ndarray:
     '''Generate galaxy redshifts from a source distribution.
+
+    The function supports sampling from multiple populations of
+    redshifts if *count* is an array or if there are additional axes in
+    the *z* or *nz* arrays.  In this case, the shape of *count* and the
+    leading dimensions of *z* and *nz* are broadcast to a common shape,
+    and redshifts are sampled independently for each extra dimension.
+    The results are concatenated into a flat array.
 
     Parameters
     ----------
-    size : int
-        Number of redshifts to sample.
+    count : int or array_like
+        Number of redshifts to sample.  If an array is given, its shape
+        is broadcast against the leading axes of *z* and *nz*.
     z, nz : array_like
-        Source distribution.  Leading axes are treated as different
-        galaxy populations.
+        Source distribution.  Leading axes are broadcast against the
+        shape of *count*.
     rng : :class:`~numpy.random.Generator`, optional
-        Random number generator.  If not given, a default RNG will be
-        used.
+        Random number generator.  If not given, a default RNG is used.
 
     Returns
     -------
-    z : array_like
-        Redshifts sampled from the given source distribution.
-    pop : array_like or None
-        Index of the galaxy population from the leading axes of ``nz``;
-        or ``None`` if there are no galaxy populations.
+    redshifts : array_like
+        Redshifts sampled from the given source distribution.  For
+        inputs with extra dimensions, returns a flattened 1-D array of
+        samples from all populations.
 
     '''
 
@@ -56,34 +64,29 @@ def redshifts_from_nz(size: int, z: np.ndarray, nz: np.ndarray, *,
     if rng is None:
         rng = np.random.default_rng()
 
-    # get galaxy populations
-    if np.ndim(nz) > 1:
-        pop = list(np.ndindex(np.shape(nz)[:-1]))
-    else:
-        pop = None
+    # bring inputs' leading axes into common shape
+    dims, count, z, nz = broadcast_leading_axes((count, 0), (z, 1), (nz, 1))
 
-    # compute the as-yet unnormalised CDF of each galaxy population
-    cdf = cumtrapz(nz, z)
+    # list of results for all dimensions
+    redshifts = np.empty(count.sum())
 
-    # compute probability to be in each galaxy population
-    p = cdf[..., -1]/cdf[..., -1].sum(axis=None, keepdims=True)
+    # keep track of the number of sampled redshifts
+    total = 0
 
-    # now normalise the CDFs
-    cdf /= cdf[..., -1:]
+    # go through extra dimensions; also works if dims is empty
+    for k in np.ndindex(dims):
 
-    # sample redshifts and populations
-    if pop is not None:
-        x = rng.choice(len(pop), p=p, size=size)
-        gal_z = rng.uniform(0, 1, size=size)
-        for i, j in enumerate(pop):
-            s = (x == i)
-            gal_z[s] = np.interp(gal_z[s], cdf[j], z)
-        gal_pop = np.take(pop, x)
-    else:
-        gal_z = np.interp(rng.uniform(0, 1, size=size), cdf, z)
-        gal_pop = None
+        # compute the CDF of each galaxy population
+        cdf = cumtrapz(nz[k], z[k], dtype=float)
+        cdf /= cdf[-1]
 
-    return gal_z, gal_pop
+        # sample redshifts and store result
+        redshifts[total:total+count[k]] = np.interp(rng.uniform(0, 1, size=count[k]), cdf, z[k])
+        total += count[k]
+
+    assert total == redshifts.size
+
+    return redshifts
 
 
 def galaxy_shear(lon: np.ndarray, lat: np.ndarray, eps: np.ndarray,
@@ -142,33 +145,34 @@ def galaxy_shear(lon: np.ndarray, lat: np.ndarray, eps: np.ndarray,
     return g
 
 
-def gaussian_phz(z: np.ndarray, sigma_0: float,
-                 rng: Optional[np.random.Generator] = None) -> np.ndarray:
+def gaussian_phz(z: ArrayLike, sigma_0: float | ArrayLike,
+                 rng: np.random.Generator | None = None) -> np.ndarray:
     r'''Photometric redshifts assuming a Gaussian error.
 
-    A simple toy model of photometric redshift errors that assumes a Gaussian
-    error with redshift-dependent standard deviation :math:`\sigma(z) = (1 + z)
-    \sigma_0` [1]_.
+    A simple toy model of photometric redshift errors that assumes a
+    Gaussian error with redshift-dependent standard deviation
+    :math:`\sigma(z) = (1 + z) \sigma_0` [1]_.
 
     Parameters
     ----------
     z : array_like
         True redshifts.
-    sigma_0 : float
+    sigma_0 : float or array_like
         Redshift error in the tomographic binning at zero redshift.
     rng : :class:`~numpy.random.Generator`, optional
-        Random number generator.  If not given, a default RNG will be used.
+        Random number generator.  If not given, a default RNG is used.
 
     Returns
     -------
     phz : array_like
-        Photometric redshifts assuming Gaussian errors, of the same shape as
-        ``z``.
+        Photometric redshifts assuming Gaussian errors, of the same
+        shape as *z*.
 
     See Also
     --------
     glass.observations.tomo_nz_gausserr :
-        Create tomographic redshift distributions assuming the same model.
+        Create tomographic redshift distributions assuming the same
+        model.
 
     References
     ----------
@@ -185,14 +189,19 @@ def gaussian_phz(z: np.ndarray, sigma_0: float,
     if rng is None:
         rng = np.random.default_rng()
 
-    size = np.shape(z)
-    z = np.reshape(z, (-1,))
+    sigma = np.add(1, z)*sigma_0
+    dims = np.shape(sigma)
 
-    zphot = rng.normal(z, (1 + z)*sigma_0)
+    zphot = rng.normal(z, sigma)
 
-    trunc = np.where(zphot < 0)[0]
-    while trunc.size:
-        zphot[trunc] = rng.normal(z[trunc], (1 + z[trunc])*sigma_0)
-        trunc = trunc[zphot[trunc] < 0]
+    if not dims:
+        while zphot < 0:
+            zphot = rng.normal(z, sigma)
+    else:
+        z = np.broadcast_to(z, dims)
+        trunc = np.where(zphot < 0)[0]
+        while trunc.size:
+            zphot[trunc] = rng.normal(z[trunc], sigma[trunc])
+            trunc = trunc[zphot[trunc] < 0]
 
-    return zphot.reshape(size)
+    return zphot
