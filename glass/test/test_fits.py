e36221ca9bbb27b3ca2c8e8600f8259a37654703
ucapbba <ucapbba@ucl.ac.uk>
2024-04-14 16:49:12 +0100

    FITS Writer class and test cases

---
+++ glass/test/test_fits.py
@@ -0,0 +1,109 @@
+import pytest
+
+# check if fitsio is available for testing
+try:
+    import fitsio
+except ImportError:
+    HAVE_FITSIO = False
+else:
+    del fitsio
+    HAVE_FITSIO = True
+
+import glass.user as user
+import numpy as np
+import fitsio
+
+
+def _test_append(fits, data, names):
+    """Write routine for FITS data."""
+    cat_name = 'CATALOG'
+    if cat_name not in fits:
+        fits.write_table(data, names=names, extname=cat_name)
+    else:
+        hdu = fits[cat_name]
+        hdu.write(data, names=names, firstrow=hdu.get_nrows())
+
+
+delta = 0.001  # Number of points in arrays
+myMax = 1000  # Typically number of galaxies in loop
+exceptInt = 750  # Where test exception occurs in loop
+filename = "MyFile.Fits"
+
+
+@pytest.mark.skipif(not HAVE_FITSIO, reason="test requires fitsio")
+def test_basic_write(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    filename_gfits = "gfits.fits"  # what GLASS creates
+    filename_tfits = "tfits.fits"  # file create on the fly to test against
+
+    with user.swrite(d / filename_gfits, ext="CATALOG") as out, fitsio.FITS(d / filename_tfits, "rw", clobber=True) as myFits:
+        for i in range(0, myMax):
+            array = np.arange(i, i+1, delta)  # array of size 1/delta
+            array2 = np.arange(i+1, i+2, delta)  # array of size 1/delta
+            out.write(RA=array, RB=array2)
+            arrays = [array, array2]
+            names = ['RA', 'RB']
+            _test_append(myFits, arrays, names)
+
+    from astropy.io import fits
+    with fits.open(d / filename_gfits) as g_fits, fits.open(d / filename_tfits) as t_fits:
+        glass_data = g_fits[1].data
+        test_data = t_fits[1].data
+        assert glass_data['RA'].size == test_data['RA'].size
+        assert glass_data['RB'].size == test_data['RA'].size
+
+
+@pytest.mark.skipif(not HAVE_FITSIO, reason="test requires fitsio")
+def test_write_exception(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+
+    try:
+        with user.swrite(d / filename, ext="CATALOG") as out:
+            for i in range(0, myMax):
+                if i == exceptInt:
+                    raise Exception("Unhandled exception")
+                array = np.arange(i, i+1, delta)  # array of size 1/delta
+                array2 = np.arange(i+1, i+2, delta)  # array of size 1/delta
+                out.write(RA=array, RB=array2)
+
+    except Exception:
+        from astropy.io import fits
+        with fits.open(d / filename) as hdul:
+            data = hdul[1].data
+            assert data['RA'].size == exceptInt/delta
+            assert data['RB'].size == exceptInt/delta
+
+            fitsMat = data['RA'].reshape(exceptInt, int(1/delta))
+            fitsMat2 = data['RB'].reshape(exceptInt, int(1/delta))
+            for i in range(0, exceptInt):
+                array = np.arange(i, i+1, delta)  # re-create array to compare to read data
+                array2 = np.arange(i+1, i+2, delta)
+                assert array.tolist() == fitsMat[i].tolist()
+                assert array2.tolist() == fitsMat2[i].tolist()
+
+
+@pytest.mark.skipif(not HAVE_FITSIO, reason="test requires fitsio")
+def test_out_filename(tmp_path):
+
+    fits = fitsio.FITS(filename, "rw", clobber=True)
+    writer = user.FitsWriter(fits)
+    assert writer.fits._filename == filename
+
+
+@pytest.mark.skipif(not HAVE_FITSIO, reason="test requires fitsio")
+def test_write_none(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    with user.swrite(d / filename, ext="CATALOG") as out:
+        out.write()
+    assert 1 == 1
+
+
+@pytest.mark.skipif(not HAVE_FITSIO, reason="test requires fitsio")
+def test_awrite_yield(tmp_path):
+    d = tmp_path / "sub"
+    d.mkdir()
+    with user.swrite(d / filename, ext="CATALOG") as out:
+        assert type(out) is user.FitsWriter
--- glass/user.py
+++ glass/user.py
@@ -10,15 +10,21 @@ The :mod:`glass.user` module contains convenience functions for users of the
 library.
 
 
-Input and output
+Basic IO
 ----------------
 
 .. autofunction:: save_cls
 .. autofunction:: load_cls
 
+FITS creation
+----------------
+.. autofunction:: swrite
+.. autoclass:: SyncHduWriter
+
 '''
 
 import numpy as np
+from contextlib import contextmanager
 
 
 def save_cls(filename, cls):
@@ -45,3 +51,45 @@ def load_cls(filename):
         values = npz['values']
         split = npz['split']
     return np.split(values, split)
+
+
+class FitsWriter:
+    """Writer that appends rows to a HDU."""
+
+    def __init__(self, fits, ext=None):
+        """Create a new, uninitialised writer."""
+        self.fits = fits
+        self.ext = ext
+
+    def _append(self, data, names=None):
+        """Write routine for FITS data."""
+
+        if self.ext is None or self.ext not in self.fits:
+            self.fits.write_table(data, names=names, extname=self.ext)
+            if self.ext is None:
+                self.ext = self.fits[-1].get_extnum()
+        else:
+            hdu = self.fits[self.ext]
+            # not using hdu.append here because of incompatibilities
+            hdu.write(data, names=names, firstrow=hdu.get_nrows())
+
+    def write(self, data=None, /, **columns):
+        """Append to FITS."""
+        # if data is given, write it as it is
+        if data is not None:
+            self._append(data)
+            
+        # if keyword arguments are given, treat them as names and columns
+        if columns:
+            names, values = list(columns.keys()), list(columns.values())
+            self._append(values, names)
+
+
+@contextmanager
+def swrite(filename, *, ext=None):
+    """Context manager for a FITS catalogue writer."""
+    import fitsio
+    with fitsio.FITS(filename, "rw", clobber=True) as fits:
+        fits.write(None)
+        writer = FitsWriter(fits, ext)
+        yield writer
