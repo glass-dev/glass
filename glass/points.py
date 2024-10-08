6883e1ca0a1070cfac5c2cb60d6e9c72b0223564
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-10-08 21:06:07 +0100

    Add space

--- glass/points.py
+++ glass/points.py
@@ -190,7 +190,7 @@ def positions_from_delta(  # noqa: PLR0912, PLR0913, PLR0915
     # iterate the leading dimensions
     for k in np.ndindex(dims):
         # compute density contrast from bias model, or copy
-        n = np.copy(delta[k]) if bias is None else bias_model(delta[k], bias[k])  # type: ignore[call-overload,operator]
+        n = np.copy(delta[k]) if bias is None else bias_model(delta[k], bias[k])  # type: ignore[call-overload, operator]
 
         # remove monopole if asked to
         if remove_monopole:
