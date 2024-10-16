fd22c2e8816661e10069328f4f7135b8c9d08b47
Saransh Chopra <saransh0701@gmail.com>
2024-10-16 11:04:09 +0100

    gh-272: add tests for glass.points

--- tests/test_points.py
+++ tests/test_points.py
@@ -1,6 +1,13 @@
 import numpy as np
+from pytest_mock import MockerFixture
 
-from glass.points import position_weights, positions_from_delta, uniform_positions
+from glass.points import (
+    linear_bias,
+    loglinear_bias,
+    position_weights,
+    positions_from_delta,
+    uniform_positions,
+)
 
 
 def catpos(pos):  # type: ignore[no-untyped-def]
@@ -10,6 +17,61 @@ def catpos(pos):  # type: ignore[no-untyped-def]
         lat = np.concatenate([lat, la])  # type: ignore[assignment]
         cnt = cnt + co
     return lon, lat, cnt
+
+
+# TODO(@Saransh-cpp): populate this function  # noqa: FIX002
+# https://github.com/glass-dev/glass/issues/272
+def test_effective_bias(mocker: MockerFixture) -> None:
+    # create a mock radial window function
+    w = mocker.Mock()
+    w.za = np.linspace(0.0, 1.0, 20)
+    w.wa = np.exp(-0.5 * (w.za - 0.5) ** 2 / 0.1**2)
+
+
+def test_linear_bias(rng: np.random.Generator) -> None:
+    # test with 0 delta
+
+    delta = np.zeros((2, 2))
+    b = 2.0
+
+    np.testing.assert_allclose(linear_bias(delta, b), np.zeros((2, 2)))  # type: ignore[no-untyped-call]
+
+    # test with 0 b
+
+    delta = rng.normal(5, 1, size=(2, 2))
+    b = 0.0
+
+    np.testing.assert_allclose(linear_bias(delta, b), np.zeros((2, 2)))  # type: ignore[no-untyped-call]
+
+    # compare with original implementation
+
+    delta = rng.normal(5, 1, size=(2, 2))
+    b = 2.0
+
+    np.testing.assert_allclose(linear_bias(delta, b), b * delta)  # type: ignore[no-untyped-call]
+
+
+def test_loglinear_bias(rng: np.random.Generator) -> None:
+    # test with 0 delta
+
+    delta = np.zeros((2, 2))
+    b = 2.0
+
+    np.testing.assert_allclose(loglinear_bias(delta, b), np.zeros((2, 2)))  # type: ignore[no-untyped-call]
+
+    # test with 0 b
+
+    delta = rng.normal(5, 1, size=(2, 2))
+    b = 0.0
+
+    np.testing.assert_allclose(loglinear_bias(delta, b), np.zeros((2, 2)))  # type: ignore[no-untyped-call]
+
+    # compare with numpy implementation
+
+    delta = rng.normal(5, 1, size=(2, 2))
+    b = 2.0
+
+    np.testing.assert_allclose(loglinear_bias(delta, b), np.expm1(b * np.log1p(delta)))  # type: ignore[no-untyped-call]
 
 
 def test_positions_from_delta():  # type: ignore[no-untyped-def]
@@ -65,12 +127,16 @@ def test_positions_from_delta():  # type: ignore[no-untyped-def]
     assert lat.shape == (cnt.sum(),)
 
 
-def test_uniform_positions():  # type: ignore[no-untyped-def]
+def test_uniform_positions(rng):  # type: ignore[no-untyped-def]
     # case: scalar input
 
     ngal = 1e-3
 
     lon, lat, cnt = catpos(uniform_positions(ngal))  # type: ignore[no-untyped-call]
+
+    # test with rng
+
+    lon, lat, cnt = catpos(uniform_positions(ngal, rng=rng))  # type: ignore[no-untyped-call]
 
     assert isinstance(cnt, int)
     assert lon.shape == lat.shape == (cnt,)
