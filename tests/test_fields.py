43b20d0ee117fea1350811850217d1a9264b224a
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-10-28 10:53:57 +0000

    Change test to satisfy `mypy`

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -5,8 +5,12 @@ from glass.fields import getcl
 
 def test_getcl() -> None:
     # make a mock Cls array with the index pairs as entries
-    cls = np.array([{i, j} for i in range(10) for j in range(i, -1, -1)])
+    cls = [
+        np.array([i, j], dtype=np.float64) for i in range(10) for j in range(i, -1, -1)
+    ]
     # make sure indices are retrieved correctly
     for i in range(10):
         for j in range(10):
-            assert getcl(cls, i, j) == {i, j}
+            result = getcl(cls, i, j)
+            expected = np.array([min(i, j), max(i, j)], dtype=np.float64)
+            np.testing.assert_array_equal(np.sort(result), expected)
