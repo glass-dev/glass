8e5962803bb0cd9178a5f54ab62a32fd7a30ab5e
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-11-18 15:57:00 +0000

    gh-271: add tests for `glass.observations`

---
+++ tests/test_observations.py
@@ -0,0 +1,22 @@
+def test_vmap_galactic_ecliptic() -> None:
+    """_summary_."""
+
+
+def test_gaussian_nz() -> None:
+    """_summary_."""
+
+
+def test_smail_nz() -> None:
+    """_summary_."""
+
+
+def test_fixed_zbins() -> None:
+    """_summary_."""
+
+
+def test_equal_dens_zbins() -> None:
+    """_summary_."""
+
+
+def test_tomo_nz_gausserr() -> None:
+    """_summary_."""
