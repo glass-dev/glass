a5cb68a876ec61354fe76762161288268fe725aa
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2024-11-18 16:09:47 +0000

    Add docstrings to all unit tests

--- tests/test_observations.py
+++ tests/test_observations.py
@@ -1,22 +1,22 @@
 def test_vmap_galactic_ecliptic() -> None:
-    """_summary_."""
+    """Add unit tests for vmap_galactic_ecliptic."""
 
 
 def test_gaussian_nz() -> None:
-    """_summary_."""
+    """Add unit tests for gaussian_nz."""
 
 
 def test_smail_nz() -> None:
-    """_summary_."""
+    """Add unit tests for smail_nz."""
 
 
 def test_fixed_zbins() -> None:
-    """_summary_."""
+    """Add unit tests for fixed_zbins."""
 
 
 def test_equal_dens_zbins() -> None:
-    """_summary_."""
+    """Add unit tests for equal_dens_zbins."""
 
 
 def test_tomo_nz_gausserr() -> None:
-    """_summary_."""
+    """Add unit tests for tomo_nz_gausserr."""
