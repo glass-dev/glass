f9ab450258cc53a1496b0231d3998adb067a22ef
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-03-17 17:02:17 +0000

    Simplify docstring

--- glass/cosmology.py
+++ glass/cosmology.py
@@ -22,12 +22,4 @@ class Cosmology(
     cosmology.api.HasTransverseComovingDistance[Array, Array],  # type: ignore[misc]
     Protocol,
 ):
-    """
-    Cosmology protocol for GLASS.
-
-    GLASS requires a cosmology object with the following methods
-    and attributes, as defined by the Cosmology API:
-    * :meth:`cosmology.api.H_over_H0`
-    * ...
-
-    """
+    """Cosmology protocol for GLASS."""
