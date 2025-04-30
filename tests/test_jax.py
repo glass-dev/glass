047d6c3c482af7c5195cdf783d0aee538cae9989
Saransh Chopra <saransh0701@gmail.com>
2025-04-30 20:45:25 +0100

    better imports and better typing

--- tests/test_jax.py
+++ tests/test_jax.py
@@ -3,19 +3,18 @@ import pytest
 import jax
 import jax.numpy as jnp
 from glass._array_api_utils import JAXGenerator
-from jax import dtypes, random
 
 
 def test_init() -> None:
     rng = JAXGenerator(42)
     assert isinstance(rng, JAXGenerator)
     assert isinstance(rng.key, jax.Array)
-    assert dtypes.issubdtype(rng.key.dtype, dtypes.prng_key)
-    assert jnp.all(rng.key == random.key(42))
+    assert jax.dtypes.issubdtype(rng.key.dtype, jax.dtypes.prng_key)
+    assert jnp.all(rng.key == jax.random.key(42))
 
 
 def test_from_key() -> None:
-    key = random.key(42)
+    key = jax.random.key(42)
     rng = JAXGenerator.from_key(key)
     assert rng.key is key
 
@@ -28,7 +27,7 @@ def test_from_key() -> None:
 
 def test_key() -> None:
     rng = JAXGenerator(42)
-    rngkey, outkey = random.split(rng.key, 2)
+    rngkey, outkey = jax.random.split(rng.key, 2)
     key = rng.split()
     assert jnp.all(rng.key == rngkey)
     assert jnp.all(key == outkey)
@@ -36,7 +35,7 @@ def test_key() -> None:
 
 def test_spawn() -> None:
     rng = JAXGenerator(42)
-    key, *subkeys = random.split(rng.key, 4)
+    key, *subkeys = jax.random.split(rng.key, 4)
     subrngs = rng.spawn(3)
     assert rng.key == key
     assert isinstance(subrngs, list)
@@ -46,7 +45,7 @@ def test_spawn() -> None:
         assert subrng.key == subkey
 
 
-def test_random():
+def test_random() -> None:
     rng = JAXGenerator(42)
     key = rng.key
     rvs = rng.random(size=10000)
@@ -56,7 +55,7 @@ def test_random():
     assert rvs.max() < 1.0
 
 
-def test_normal():
+def test_normal() -> None:
     rng = JAXGenerator(42)
     key = rng.key
     rvs = rng.normal(1, 2, size=10000)
@@ -64,7 +63,7 @@ def test_normal():
     assert rvs.shape == (10000,)
 
 
-def test_standard_normal():
+def test_standard_normal() -> None:
     rng = JAXGenerator(42)
     key = rng.key
     rvs = rng.standard_normal(size=10000)
@@ -72,7 +71,7 @@ def test_standard_normal():
     assert rvs.shape == (10000,)
 
 
-def test_poisson():
+def test_poisson() -> None:
     rng = JAXGenerator(42)
     key = rng.key
     rvs = rng.poisson(lam=1, size=10000)
@@ -80,7 +79,7 @@ def test_poisson():
     assert rvs.shape == (10000,)
 
 
-def test_uniform():
+def test_uniform() -> None:
     rng = JAXGenerator(42)
     key = rng.key
     rvs = rng.uniform(size=10000)
