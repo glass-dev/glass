3ee03ab385425070a722b876987046520b5a2c5b
Saransh Chopra <saransh0701@gmail.com>
2025-05-01 16:55:23 +0100

    use scope="session"
    Co-authored-by: Patrick J. Roddy <patrickjamesroddy@gmail.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -118,7 +118,7 @@ def xp(request: pytest.FixtureRequest) -> types.ModuleType:
     return request.param
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def urng(xp: types.ModuleType) -> UnifiedGenerator:
     """
     Fixture for a unified RNG interface.
