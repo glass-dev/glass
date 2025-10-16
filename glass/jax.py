936661ecb44277b76bdb6c3823d8d77007655710
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-10-16 17:27:24 +0100

    Move all imports to own function

--- glass/jax.py
+++ glass/jax.py
@@ -6,7 +6,6 @@ import math
 from threading import Lock
 from typing import TYPE_CHECKING, TypeAlias
 
-from jax.typing import ArrayLike
 from typing_extensions import Self
 
 if TYPE_CHECKING:
@@ -34,16 +33,14 @@ def _size(size: Size, *bcast: Array) -> tuple[int, ...]:
     return size
 
 
-def trapezoid(
-    y: ArrayLike, x: ArrayLike = None, dx: ArrayLike = 1.0, axis: int = -1
-) -> Array:
+def trapezoid(y: Array, x: Array = None, dx: Array = 1.0, axis: int = -1) -> Array:
     """Wrapper for jax.scipy.integrate.trapezoid."""
     import jax.scipy  # noqa: PLC0415
 
     return jax.scipy.integrate.trapezoid(y, x=x, dx=dx, axis=axis)
 
 
-def union1d(ar1: ArrayLike, ar2: ArrayLike) -> Array:
+def union1d(ar1: Array, ar2: Array) -> Array:
     """Wrapper for jax.numpy.trapezoid."""
     import jax.numpy as jnp  # noqa: PLC0415
 
@@ -51,12 +48,12 @@ def union1d(ar1: ArrayLike, ar2: ArrayLike) -> Array:
 
 
 def interp(  # noqa: PLR0913
-    x: ArrayLike,
-    x_points: ArrayLike,
-    y_points: ArrayLike,
-    left: ArrayLike = None,
-    right: ArrayLike = None,
-    period: ArrayLike = None,
+    x: Array,
+    x_points: Array,
+    y_points: Array,
+    left: Array = None,
+    right: Array = None,
+    period: Array = None,
 ) -> Array:
     """Wrapper for jax.numpy.interp."""
     import jax.numpy as jnp  # noqa: PLC0415
@@ -64,7 +61,7 @@ def interp(  # noqa: PLR0913
     return jnp.interp(x, x_points, y_points, left=left, right=right, period=period)
 
 
-def gradient(f: ArrayLike) -> Array:
+def gradient(f: Array) -> Array:
     """Wrapper for jax.numpy.gradient."""
     import jax.numpy as jnp  # noqa: PLC0415
 
@@ -72,7 +69,7 @@ def gradient(f: ArrayLike) -> Array:
 
 
 def linalg_lstsq(
-    a: ArrayLike, b: ArrayLike, rcond: float | None = None
+    a: Array, b: Array, rcond: float | None = None
 ) -> tuple[Array, Array, Array, Array]:
     """Wrapper for jax.numpy.linalg.lstsq."""
     import jax.numpy as jnp  # noqa: PLC0415
@@ -80,7 +77,7 @@ def linalg_lstsq(
     return jnp.linalg.lstsq(a, b, rcond)  # type: ignore[no-any-return]
 
 
-def einsum(subscripts: str, *operands: ArrayLike) -> Array:
+def einsum(subscripts: str, *operands: Array) -> Array:
     """Wrapper for jax.numpy.einsum."""
     import jax.numpy as jnp  # noqa: PLC0415
 
@@ -90,7 +87,7 @@ def einsum(subscripts: str, *operands: ArrayLike) -> Array:
 def apply_along_axis(
     func1d: FunctionType,
     axis: int,
-    arr: ArrayLike,
+    arr: Array,
     *args: object,
     **kwargs: object,
 ) -> Array:
@@ -103,9 +100,6 @@ def apply_along_axis(
 class Generator:
     """JAX random number generation as a NumPy generator."""
 
-    import jax.dtypes  # noqa: PLC0415
-    import jax.random  # noqa: PLC0415
-
     __slots__ = ("key", "lock")
     key: PRNGKeyArray
     lock: Lock
@@ -113,8 +107,11 @@ class Generator:
     @classmethod
     def from_key(cls, key: PRNGKeyArray) -> Self:
         """Wrap a JAX random key."""
-        if not isinstance(key, ArrayLike) or not cls.jax.dtypes.issubdtype(
-            key.dtype, cls.jax.dtypes.prng_key
+        import jax.dtypes  # noqa: PLC0415
+        from jax.typing import ArrayLike  # noqa: PLC0415
+
+        if not isinstance(key, ArrayLike) or not jax.dtypes.issubdtype(
+            key.dtype, jax.dtypes.prng_key
         ):
             msg = "not a random key"
             raise ValueError(msg)
@@ -125,33 +122,43 @@ class Generator:
 
     def __init__(self, seed: int | Array, *, impl: str | None = None) -> None:
         """Create a wrapper instance with a new key."""
-        self.key = self.jax.random.key(seed, impl=impl)
+        import jax.random  # noqa: PLC0415
+
+        self.key = jax.random.key(seed, impl=impl)
         self.lock = Lock()
 
     @property
     def __key(self) -> Array:
         """Return next key for sampling while updating internal state."""
+        import jax.random  # noqa: PLC0415
+
         with self.lock:
-            self.key, key = self.jax.random.split(self.key)
+            self.key, key = jax.random.split(self.key)
         return key
 
     def split(self, size: Size = None) -> Array:
         """Split random key."""
+        import jax.random  # noqa: PLC0415
+
         shape = _size(size)
         with self.lock:
-            keys = self.jax.random.split(self.key, 1 + math.prod(shape))
+            keys = jax.random.split(self.key, 1 + math.prod(shape))
             self.key = keys[0]
         return keys[1:].reshape(shape)
 
     def spawn(self, n_children: int) -> list[Self]:
         """Create new independent child generators."""
+        import jax.random  # noqa: PLC0415
+
         with self.lock:
-            self.key, *keys = self.jax.random.split(self.key, num=n_children + 1)
+            self.key, *keys = jax.random.split(self.key, num=n_children + 1)
         return list(map(self.from_key, keys))
 
     def random(self, size: Size = None, dtype: Shaped[Array, ...] = float) -> Array:
         """Return random floats in the half-open interval [0.0, 1.0)."""
-        return self.jax.random.uniform(self.__key, _size(size), dtype)
+        import jax.random  # noqa: PLC0415
+
+        return jax.random.uniform(self.__key, _size(size), dtype)
 
     def normal(
         self,
@@ -161,19 +168,25 @@ class Generator:
         dtype: Shaped[Array, ...] = float,
     ) -> Array:
         """Draw samples from a Normal distribution (mean=loc, stdev=scale)."""
-        return loc + scale * self.jax.random.normal(self.__key, _size(size), dtype)
+        import jax.random  # noqa: PLC0415
+
+        return loc + scale * jax.random.normal(self.__key, _size(size), dtype)
 
     def poisson(
         self, lam: float, size: Size = None, dtype: Integer[Array, ...] = int
     ) -> Array:
         """Draw samples from a Poisson distribution."""
-        return self.jax.random.poisson(self.__key, lam, size, dtype)
+        import jax.random  # noqa: PLC0415
+
+        return jax.random.poisson(self.__key, lam, size, dtype)
 
     def standard_normal(
         self, size: Size = None, dtype: Shaped[Array, ...] = float
     ) -> Array:
         """Draw samples from a standard Normal distribution (mean=0, stdev=1)."""
-        return self.jax.random.normal(self.__key, _size(size), dtype)
+        import jax.random  # noqa: PLC0415
+
+        return jax.random.normal(self.__key, _size(size), dtype)
 
     def uniform(
         self,
@@ -183,4 +196,6 @@ class Generator:
         dtype: Shaped[Array, ...] = float,
     ) -> Array:
         """Draw samples from a Uniform distribution."""
-        return self.jax.random.uniform(self.__key, _size(size), dtype, low, high)
+        import jax.random  # noqa: PLC0415
+
+        return jax.random.uniform(self.__key, _size(size), dtype, low, high)
