28ff98f051d89d70af6f1eb438ad9fe75716cd0b
connoraird <c.aird@ucl.ac.uk>
2025-10-22 12:58:37 +0100

    Add test of nnls which covers full function

--- tests/test_algorithm.py
+++ tests/test_algorithm.py
@@ -38,6 +38,29 @@ def test_nnls(xp: types.ModuleType, urng: UnifiedGenerator) -> None:
     )
     np.testing.assert_allclose(res, b, rtol=0.0, atol=1e-10)
 
+    # Force output to be different from b so we cover more of nnls
+
+    # a is 0.0->50.0 with a negative number instead of multiples of 7
+    a = xp.arange(0.0, 500.0, 1.0)
+    a = xp.where((xp.astype(a, xp.int64) % 7) == 0, -1.1781, a)
+    a = xp.reshape(a, (50, 10))
+    # b is 0.0->10.0 with zero instead of multiples of 2
+    b = xp.arange(0.0, 10.0, 1.0)
+    b = xp.where((xp.astype(b, xp.int64) % 2) == 0, b, 0.0)
+    x = a @ b
+    res = glass.algorithm.nnls(
+        a,
+        x,
+        tol=500 * xp.linalg.matrix_norm(a, ord=1) * xp.finfo(xp.float64).eps,
+    )
+    np.testing.assert_allclose(
+        res,
+        xp.asarray(
+            [0.0, 0.0, 0.0, 0.0, 4.002368, 0.0, 5.998561, 0.0, 8.000571, 1.957038]
+        ),
+        rtol=1e-6,
+    )
+
     # check matrix and vector's shape
 
     a = urng.standard_normal((100, 20))
