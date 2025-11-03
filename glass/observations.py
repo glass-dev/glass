9e103ae1af2501f2d0b1946d625e876b3589c9e6
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-11-03 12:25:23 +0000

    No longer default to NumPy

--- glass/observations.py
+++ glass/observations.py
@@ -209,7 +209,7 @@ def fixed_zbins(
     *,
     nbins: int | None = None,
     dz: float | None = None,
-    xp: ModuleType | None = None,
+    xp: ModuleType,
 ) -> list[tuple[float, float]]:
     """
     Tomographic redshift bins of fixed size.
@@ -240,8 +240,6 @@ def fixed_zbins(
         If both ``nbins`` and ``dz`` are given.
 
     """
-    xp = np if xp is None else xp
-
     if nbins is not None and dz is None:
         zbinedges = xp.linspace(zmin, zmax, nbins + 1)
     elif nbins is None and dz is not None:
