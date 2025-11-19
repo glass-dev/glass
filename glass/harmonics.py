92329ab3f7bd0aa7b55b673d1544ec544872fb40
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2025-11-19 11:52:44 +0000

    Fix docs

--- glass/harmonics.py
+++ glass/harmonics.py
@@ -19,15 +19,15 @@ def multalm(
     """
     Multiply alm by bl.
 
-    The alm should be in GLASS order:
+    The alm should be in GLASS order::
 
-    [
-        00,
-        10, 11,
-        20, 21, 22,
-        30, 31, 32, 33,
-        ...
-    ]
+        [
+            00,
+            10, 11,
+            20, 21, 22,
+            30, 31, 32, 33,
+            ...
+        ]
 
     Parameters
     ----------
@@ -35,8 +35,6 @@ def multalm(
         The alm to multiply.
     bl
         The bl to multiply.
-    inplace
-        Whether to perform the operation in place.
 
     Returns
     -------
