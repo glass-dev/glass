5e5c8168f024ba9824e4476f88169d5370f92f75
connoraird <c.aird@ucl.ac.uk>
2025-11-20 16:43:14 +0000

    Remove test_getcl_no_lmax as this is essentially just array indexing

--- tests/benchmarks/test_fields.py
+++ tests/benchmarks/test_fields.py
@@ -13,36 +13,6 @@ if TYPE_CHECKING:
     from pytest_benchmark.fixture import BenchmarkFixture
 
 
-def test_getcl_no_lmax(
-    xp: ModuleType,
-    benchmark: BenchmarkFixture,
-    benchmark_scale_factor: int,
-) -> None:
-    """Benchmarks for glass.fields.getcl with no value of lmax."""
-    range_size = benchmark_scale_factor * 10
-    # make a mock Cls array with the index pairs as entries
-    cls = [
-        xp.asarray([i, j], dtype=xp.float64)
-        for i in range(range_size)
-        for j in range(i, -1, -1)
-    ]
-
-    random_i = randrange(range_size)
-    random_j = randrange(range_size)
-
-    # make sure indices are retrieved correctly
-    result = benchmark(
-        glass.fields.getcl,
-        cls,
-        random_i,
-        random_j,
-    )
-    expected = xp.asarray(
-        [min(random_i, random_j), max(random_i, random_j)], dtype=xp.float64
-    )
-    np.testing.assert_allclose(xp.sort(result), expected)
-
-
 def test_getcl_lmax_0(
     xp: ModuleType,
     benchmark: BenchmarkFixture,
