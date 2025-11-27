fa1a0813c16e0165838f5a529ca8b12a4d0def68
connoraird <c.aird@ucl.ac.uk>
2025-11-27 11:23:19 +0000

    Add benchmark for shapes.ellipticity_gaussian

--- tests/benchmarks/test_shapes.py
+++ tests/benchmarks/test_shapes.py
@@ -38,3 +38,28 @@ def test_ellipticity_ryden04(
         glass.shapes.ellipticity_ryden04, mu, sigma, gamma, sigma_gamma, size=size
     )
     assert e.shape == size
+
+
+@pytest.mark.stable
+def test_ellipticity_gaussian(
+    benchmark: BenchmarkFixture,
+    xp: ModuleType,
+) -> None:
+    """Benchmark for glass.shapes.ellipticity_guassian."""
+    if xp.__name__ == "jax.numpy":
+        pytest.skip(
+            "Arrays in ellipticity_gaussian are not immutable, so do not support jax",
+        )
+
+    array_length = 10
+    n = 1_000_000
+    count = xp.full(array_length, fill_value=n)
+    sigma = xp.full(array_length, fill_value=0.256)
+
+    eps = benchmark(
+        glass.ellipticity_gaussian,
+        count,
+        sigma,
+    )
+
+    assert eps.shape == (n * array_length,)
