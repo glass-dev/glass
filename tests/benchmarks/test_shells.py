3926b0a0edb4811b1ddc4c2e9d277f1d041c6501
connoraird <c.aird@ucl.ac.uk>
2025-11-27 13:45:46 +0000

    Mark test_radialwindow as unstable and rescale to be sub ms to keep variations below threshold

--- tests/benchmarks/test_shells.py
+++ tests/benchmarks/test_shells.py
@@ -13,7 +13,7 @@ if TYPE_CHECKING:
     from pytest_benchmark.fixture import BenchmarkFixture
 
 
-@pytest.mark.stable
+@pytest.mark.unstable
 def test_radialwindow(
     benchmark: BenchmarkFixture,
     xp: ModuleType,
@@ -21,8 +21,8 @@ def test_radialwindow(
 ) -> None:
     """Benchmark for shells.RadialWindow."""
     # check zeff is computed when not provided
-    arr_length = 1_000_000
-    expected_zeff = 666666.0
+    arr_length = 100_000
+    expected_zeff = 66666.0
 
     wa = xp.arange(arr_length)
     za = xp.arange(arr_length)
