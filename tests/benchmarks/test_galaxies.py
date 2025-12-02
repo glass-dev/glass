26e90b360139618550a6d1115319730e83201a36
connoraird <c.aird@ucl.ac.uk>
2025-12-02 12:20:41 +0000

    Add benchmark for galaxies.redshifts_from_nz

--- tests/benchmarks/test_galaxies.py
+++ tests/benchmarks/test_galaxies.py
@@ -30,3 +30,30 @@ def test_redshifts(
     assert z.shape == (13 * scale_factor,)
     assert xp.min(z) >= 0.0
     assert xp.max(z) <= 1.0
+
+
+def test_redshifts_from_nz(
+    benchmark: BenchmarkFixture,
+    xp: ModuleType,
+) -> None:
+    """Benchmark for galaxies.redshifts_from_nz."""
+    if xp.__name__ == "jax.numpy":
+        pytest.skip(
+            "Arrays in redshifts_from_nz are not immutable, so do not support jax",
+        )
+    scale_factor = 1_000
+    # create a mock radial window function
+    za = xp.linspace(0.0, 1.0, 20 * scale_factor)
+    wa = xp.exp(-0.5 * (za - 0.5) ** 2 / 0.1**2)
+
+    # sample redshifts (scalar)
+    redshifts = benchmark(
+        glass.redshifts_from_nz,
+        13 * scale_factor,
+        za,
+        wa,
+    )
+    assert redshifts.shape == (13 * scale_factor,)
+    assert xp.min(redshifts) >= 0.0
+    assert xp.max(redshifts) <= 1.0
+    assert xp.all((0 <= redshifts) & (redshifts <= 1))  # noqa: SIM300
