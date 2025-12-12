c7c352eac14843b14f56c41011bd18fc0dd6e0bf
connoraird <c.aird@ucl.ac.uk>
2025-12-12 15:58:33 +0000

    Revert nside value in test_positions_from_delta as I don't think this was the issue

--- tests/benchmarks/test_points.py
+++ tests/benchmarks/test_points.py
@@ -47,7 +47,7 @@ def test_positions_from_delta(  # noqa: PLR0913
             f"glass.lensing.multi_plane_matrix not yet ported for {xpb.__name__}",
         )
     # create maps that saturate the batching in the function
-    nside = 256
+    nside = 128
     npix = 12 * nside * nside
 
     ngal = xpb.asarray([1e-3, 2e-3])
