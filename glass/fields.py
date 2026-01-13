edd338a7f4b16ec1bc3b2120003b249d911aa270
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-01-13 16:42:55 +0000

    Port

--- glass/fields.py
+++ glass/fields.py
@@ -867,13 +867,15 @@ def generate(
         Sampled random fields.
 
     """
+    xp = array_api_compat.array_namespace(*gls, use_compat=False)
+
     n = len(fields)
     if len(gls) != n * (n + 1) // 2:  # type: ignore[arg-type]
         msg = "mismatch between number of fields and gls"
         raise ValueError(msg)
 
     variances = (cltovar(getcl(gls, i, i)) for i in range(n))
-    grf = _generate_grf(gls, nside, ncorr=ncorr, rng=rng)
+    grf = _generate_grf(xp.asarray(gls), nside, ncorr=ncorr, rng=rng)
 
     for t, x, var in zip(fields, grf, variances, strict=True):
         yield t(x, var)
