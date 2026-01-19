8fcba8e1a9483550b4414dcda13efe933f0db924
Patrick J. Roddy <patrickjamesroddy@gmail.com>
2026-01-19 17:48:48 +0000

    Add missing n_side

--- tests/fixtures/helper_classes.py
+++ tests/fixtures/helper_classes.py
@@ -152,9 +152,7 @@ class HealpixInputs:
     def alm(*, rng: UnifiedGenerator) -> FloatArray:
         """Generate random alm coefficients."""
         size = hp.Alm.getsize(HealpixInputs.lmax)
-        return rng.standard_normal(size) + 1.0j * rng.standard_normal(
-            size
-        )
+        return rng.standard_normal(size) + 1.0j * rng.standard_normal(size)
 
     @staticmethod
     def ipix(*, rng: UnifiedGenerator, xp: ModuleType) -> IntArray:
