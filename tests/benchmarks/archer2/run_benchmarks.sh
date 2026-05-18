2fa32e5d824b9c31558550ff9b8e32fcdc2c5401
connoraird <c.aird@ucl.ac.uk>
2026-05-18 15:06:55 +0100

    Fix typos and paths and use --active in uv sync command

--- tests/benchmarks/archer2/run_benchmarks.sh
+++ tests/benchmarks/archer2/run_benchmarks.sh
@@ -6,7 +6,7 @@ BENCHMARKS_DIR="$GLASS_DIR/tests/benchmarks"
 CPU_OR_GPU="cpu|gpu" # <-- Select one
 
 BASE_VENV=".venv-base"
-HEAD_VENV=".venv-base"
+HEAD_VENV=".venv-head"
 
 # Set the base and head refs to be compared
 BASE_REF="<some-git-commit-hash>"
@@ -17,9 +17,9 @@ rm -rf "${GLASS_DIR:?}/$BASE_VENV" # Cleanup old venv
 uv venv "$GLASS_DIR/$BASE_VENV"
 source "$GLASS_DIR/$BASE_VENV/bin/activate"
 if [ $CPU_OR_GPU = "gpu" ]; then
-  uv sync --group test --group archer2-gpu
+  uv sync --active --group test --group archer2-gpu
 else
-  uv sync --group test
+  uv sync --active --group test
 fi
 uv pip uninstall glass -y # Make sure no installation of glass already exists
 uv pip install "git+$GLASS_REPO_URL@$BASE_REF"
@@ -29,9 +29,9 @@ rm -rf "${GLASS_DIR:?}/$HEAD_VENV" # Cleanup old venv
 uv venv "$GLASS_DIR/$HEAD_VENV"
 source "$GLASS_DIR/$HEAD_VENV/bin/activate"
 if [ $CPU_OR_GPU = "gpu" ]; then
-  uv sync --group test --group archer2-gpu
+  uv sync --active --group test --group archer2-gpu
 else
-  uv sync --group test
+  uv sync --active --group test
 fi
 uv pip uninstall glass -y # Make sure no installation of glass already exists
 uv pip install "git+$GLASS_REPO_URL@$HEAD_REF"
@@ -40,4 +40,4 @@ uv pip install "git+$GLASS_REPO_URL@$HEAD_REF"
 rm -rf "$BENCHMARKS_DIR/outputs"
 
 # Submit job
-sbatch "submission_script-$CPU_OR_GPU.sh" "$GLASS_DIR"
+sbatch "$BENCHMARKS_DIR/archer2/submission_script_$CPU_OR_GPU.sh" "$GLASS_DIR"
