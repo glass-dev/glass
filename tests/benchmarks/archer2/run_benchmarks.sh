f0a844cf0424604e37e4db4a4c98104b10c7e234
connoraird <c.aird@ucl.ac.uk>
2026-05-18 15:22:19 +0100

    Improve the interface to the regression test script

--- tests/benchmarks/archer2/run_benchmarks.sh
+++ tests/benchmarks/archer2/run_benchmarks.sh
@@ -3,38 +3,97 @@
 GLASS_DIR=""
 GLASS_REPO_URL="https://github.com/glass-dev/glass"
 BENCHMARKS_DIR="$GLASS_DIR/tests/benchmarks"
-CPU_OR_GPU="cpu|gpu" # <-- Select one
+CPU_OR_GPU="cpu"
+START_REF=""
+END_REF=""
+START_VENV=".venv-start"
+END_VENV=".venv-end"
 
-BASE_VENV=".venv-base"
-HEAD_VENV=".venv-head"
+help() {
+  echo "Usage:"
+  echo "    $0 "
+  echo ""
+  echo "ARGS:"
+  echo "    -h | --help                  Display this help message."
+  echo "    -d | --glass-dir <glass/dir> Path to the cloned glass directory."
+  echo "    -s | --start-ref <start_ref> The git ref to be used as the initial state."
+  echo "    -e | --end-ref <end_ref>     The git ref to be used as the final state."
+  echo "    -g | --gpu                   Flag to state the gpu benchmark should be ran."
+}
 
-# Set the base and head refs to be compared
-BASE_REF="<some-git-commit-hash>"
-HEAD_REF="<some-git-commit-hash>"
+# check for no input arguments and show help
+if [ $# -eq 0 ];
+then
+    help
+    exit 1
+fi
+
+# parse input arguments
+while [ $# -gt 0 ] ; do
+    case $1 in
+        -h | --help)
+            help
+            exit 0
+            ;;
+        -d | --glass-dir)
+            GLASS_DIR=$2
+            shift 2
+            continue
+            ;;
+        -s | --start-ref)
+            START_REF=$2
+            shift 2
+            continue
+            ;;
+        -e | --end-ref)
+            END_REF=$2
+            shift 2
+            continue
+            ;;
+        -g | --gpu)
+            CPU_OR_GPU="gpu"
+            shift 1
+            continue
+            ;;
+        *)
+            echo "Invalid option: $1" >&2;
+            help
+            exit 1
+            ;;
+    esac
+    shift 1
+done
+
+if [[ "$START_REF" == "" || "$END_REF" == "" ]]
+then
+  echo "START_REF and END_REF must be provided"
+  help
+  exit 1
+fi
 
 # Setup base environment
-rm -rf "${GLASS_DIR:?}/$BASE_VENV" # Cleanup old venv
-uv venv "$GLASS_DIR/$BASE_VENV"
-source "$GLASS_DIR/$BASE_VENV/bin/activate"
+rm -rf "${GLASS_DIR:?}/$START_VENV" # Cleanup old venv
+uv venv "$GLASS_DIR/$START_VENV"
+source "$GLASS_DIR/$START_VENV/bin/activate"
 if [ $CPU_OR_GPU = "gpu" ]; then
   uv sync --active --group test --group archer2-gpu
 else
   uv sync --active --group test
 fi
 uv pip uninstall glass -y # Make sure no installation of glass already exists
-uv pip install "git+$GLASS_REPO_URL@$BASE_REF"
+uv pip install "git+$GLASS_REPO_URL@$START_REF"
 
 # Setup head environment
-rm -rf "${GLASS_DIR:?}/$HEAD_VENV" # Cleanup old venv
-uv venv "$GLASS_DIR/$HEAD_VENV"
-source "$GLASS_DIR/$HEAD_VENV/bin/activate"
+rm -rf "${GLASS_DIR:?}/$END_VENV" # Cleanup old venv
+uv venv "$GLASS_DIR/$END_VENV"
+source "$GLASS_DIR/$END_VENV/bin/activate"
 if [ $CPU_OR_GPU = "gpu" ]; then
   uv sync --active --group test --group archer2-gpu
 else
   uv sync --active --group test
 fi
 uv pip uninstall glass -y # Make sure no installation of glass already exists
-uv pip install "git+$GLASS_REPO_URL@$HEAD_REF"
+uv pip install "git+$GLASS_REPO_URL@$END_REF"
 
 # Remove old benchmark results
 rm -rf "$BENCHMARKS_DIR/outputs"
