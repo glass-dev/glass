4291949b4d157fc57113c56616db9d09f394cc78
Nicolas Tessore <n.tessore@ucl.ac.uk>
2026-05-21 17:58:16 +0100

    update benchmark tests

--- tests/benchmarks/test_fields.py
+++ tests/benchmarks/test_fields.py
@@ -25,25 +25,17 @@ def test_iternorm_no_size(
     generator_consumer: GeneratorConsumer,
     xpb: ModuleType,
 ) -> None:
-    """Benchmarks for glass.iternorm with default value for size."""
-    k = 2
-    array_in = [xpb.asarray(x) for x in xpb.arange(10_000, dtype=xpb.float64)]
+    """Benchmarks for glass.iternorm with k=2."""
+    array_in = [xpb.asarray([x, x, x]) for x in xpb.arange(10_000, dtype=xpb.float64)]
 
-    def function_to_benchmark() -> list[Any]:
-        generator = glass.iternorm(k, iter(array_in))
-        return generator_consumer.consume(
+    def function_to_benchmark() -> None:
+        generator = glass.iternorm(array_in)
+        generator_consumer.consume(
             generator,
             valid_exception="covariance matrix is not positive definite",
         )
 
-    results = benchmark(function_to_benchmark)
-    j, a, s = results[0]
-
-    assert isinstance(j, int)
-    assert a.shape == (k,)
-    assert s.shape == ()
-    assert s.dtype == xpb.float64
-    assert s.shape == ()
+    benchmark(function_to_benchmark)
 
 
 @pytest.mark.stable
@@ -54,9 +46,7 @@ def test_iternorm_specify_size(
     xpb: ModuleType,
     num_dimensions: int,
 ) -> None:
-    """Benchmarks for glass.iternorm with size specified."""
-    k = 2
-    size = (3,)
+    """Benchmarks for glass.iternorm with k=2 and size=(3,)."""
     if num_dimensions == 1:
         list_input = [[1.0, 0.5, 0.5] for _ in range(10_000)]
     elif num_dimensions == 2:
@@ -69,57 +59,31 @@ def test_iternorm_specify_size(
             for _ in range(10_000)
         ]
     array_in = [xpb.asarray(arr, dtype=xpb.float64) for arr in list_input]
-    expected_result = [
-        1,
-        (3, 2),
-        (3,),
-    ]
 
-    def function_to_benchmark() -> list[Any]:
-        generator = glass.iternorm(k, iter(array_in), size)
-        return generator_consumer.consume(
+    def function_to_benchmark() -> None:
+        generator = glass.iternorm(array_in)
+        generator_consumer.consume(
             generator,
             valid_exception="covariance matrix is not positive definite",
         )
 
-    # check output shapes and types
-
-    results = benchmark(function_to_benchmark)
-    result1 = results[0]
-    result2 = results[1]
-
-    assert result1 != result2
-    assert isinstance(result1, tuple)
-    assert len(result1) == 3
-
-    j, a, s = result1
-    assert isinstance(j, int)
-    assert j == expected_result[0]
-    assert a.shape == expected_result[1]
-    assert s.shape == expected_result[2]
+    benchmark(function_to_benchmark)
 
 
 @pytest.mark.stable
 def test_iternorm_k_0(
     benchmark: BenchmarkFixture,
-    compare: Compare,
     generator_consumer: GeneratorConsumer,
     xpb: ModuleType,
 ) -> None:
     """Benchmarks for glass.iternorm with k set to 0."""
-    k = 0
-    array_in = [xpb.stack([x]) for x in xpb.ones(1_000, dtype=xpb.float64)]
+    array_in = [xpb.asarray([x]) for x in xpb.ones(1_000, dtype=xpb.float64)]
 
-    def function_to_benchmark() -> list[Any]:
-        generator = glass.iternorm(k, iter(array_in))
-        return generator_consumer.consume(generator)
+    def function_to_benchmark() -> None:
+        generator = glass.iternorm(array_in)
+        generator_consumer.consume(generator)
 
-    results = benchmark(function_to_benchmark)
-
-    j, a, s = results[0]
-    assert j is None
-    assert a.shape == (0,)
-    compare.assert_allclose(xpb.asarray(s), 1.0)
+    benchmark(function_to_benchmark)
 
 
 @pytest.mark.stable
