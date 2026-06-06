e15925d596cd202e6f02bc9e9fcc853d68971815
Nicolas Tessore <n.tessore@ucl.ac.uk>
2026-06-06 19:19:40 +0100

    regression test for distribute()

--- tests/regression/test_shells.py
+++ tests/regression/test_shells.py
@@ -30,3 +30,19 @@ def test_radialwindow(
     w = benchmark(glass.RadialWindow, za, wa)
 
     xpx.testing.assert_close(w.zeff, expected_zeff)
+
+
+def test_distribute(
+    benchmark: BenchmarkFixture,
+    xpb: ModuleType,
+) -> None:
+    """Regression test for distribute() over a moderately-sized catalogue."""
+    # use 50 shells
+    shells = glass.linear_windows(xpb.linspace(0.0, 3.0, 52))
+    assert len(shells) == 50
+    # use 1 million redshifts
+    redshifts = xpb.linspace(0.1, 2.9, 1_000_000)
+    # distribute redshifts over shells; problem size is 50_000_000
+    result = benchmark(glass.distribute, redshifts, shells)
+    # make sure result was computed for each redshift
+    assert result.shape == redshifts.shape
