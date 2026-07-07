4022840da6e265450ff57fbfd870d76825e9940e
connoraird <c.aird@ucl.ac.uk>
2026-07-07 14:32:16 +0100

    Revert "define xp with only numpy directly in regression test conftest file"
    This reverts commit 2356c040cc7ecd878522782cc62d10088b509e89.

--- tests/fixtures/array_backends.py
+++ tests/fixtures/array_backends.py
@@ -108,6 +108,34 @@ def xp(request: pytest.FixtureRequest) -> ModuleType:
 
 
 @pytest.fixture(
+    params=[
+        xp
+        for name, xp in xp_available_backends.items()
+        if name not in {"array_api_strict", "jax.numpy"}
+    ],
+    scope="session",
+)
+def xpb(request: pytest.FixtureRequest) -> ModuleType:
+    """
+    Fixture for array backend to be used in regression tests.
+
+    Access array library functions using `xpb.` in tests.
+
+    We are excluding array-api-strict and jax for two reasons
+    1. Our use of array-api-strict is not for its performance but
+       for checking our interface with array libraries. Additionally,
+       users are unlikely to use array-api-strict with glass.
+       Therefore, it is not worth regression testing with array-api-strict.
+    2. We did not previously support jax, therefore it does
+       not _yet_ make sense to regression test jax as there is
+       nothing to compare against, since jax is not supported by
+       the older versions of glass.
+
+    """
+    return request.param
+
+
+@pytest.fixture(
     params=[xp for name, xp in xp_available_backends.items() if name != "jax.numpy"],
     scope="session",
 )
