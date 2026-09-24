57ba383c6882517a710d1a970aa85df64f2fb332
connoraird <c.aird@ucl.ac.uk>
2026-09-24 09:51:04 +0100

    Align submit-cpu with submit-gpu

--- benchmarks/archer2/submit-cpu.sh
+++ benchmarks/archer2/submit-cpu.sh
@@ -3,12 +3,18 @@
 
 #SBATCH --job-name=glass_benchmark_cpu
 #SBATCH --output=%x-%j.out
+#SBATCH --cpus-per-task=1
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
 #SBATCH --time=0:30:0
 #SBATCH --partition=standard
 #SBATCH --qos=standard
+
+# Recommended environment settings
+# Stop unintentional multi-threading within software libraries
+export OMP_NUM_THREADS=1
+# Ensure the cpus-per-task option is propagated to srun commands
+export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
 GLASS_DIR=""
 ARRAY_BACKEND="numpy"
@@ -26,14 +32,6 @@ help() {
   echo "    --healpy-datapath <healpy-datapath>  The path to the healpy-data repo to allow"
   echo "                                         running offline. Defaults to <glass/dir>/healpy-data"
 }
-
-# Ensure uv is available
-source "${HOME/home/work}/.profile" # HOME starts as /home/... but uv needs to be on /work/...
-
-# Stop unintentional multi-threading within software libraries
-export OMP_NUM_THREADS=1
-# Ensure the cpus-per-task option is propagated to srun commands
-export SRUN_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
 
 # check for no input arguments and show help
 if [ $# -eq 0 ];
@@ -85,19 +83,11 @@ if [[ "$HEALPY_DATAPATH" == "" ]]; then
     HEALPY_DATAPATH="$GLASS_DIR/healpy-data"
 fi
 
-# Get execution method
-PYTHON_COMMAND="$GLASS_DIR/.venv/bin/python"
-if [[ -n "$SLURM_JOB_ID" || -n "$SLURM_BATCH_SCRIPT" ]]; then
-  echo "Running under SLURM (batch). SLURM_JOB_ID=${SLURM_JOB_ID:-unknown}"
-  PYTHON_COMMAND="srun $PYTHON_COMMAND"
-else
-  echo "Running directly from CLI"
-fi
-
-for n in {128,256,512,1024}
+for n in {128,256,512,1024,2048}
 do
     echo "Running benchmark with nside/lmax = $n"
     sed -i -E "s/nside = lmax = [0-9]+/nside = lmax = $n/g" "$GLASS_DIR/benchmarks/lensing.py"
 
-    ARRAY_BACKEND="$ARRAY_BACKEND" HEALPY_DATAPATH="$HEALPY_DATAPATH" $PYTHON_COMMAND "$GLASS_DIR/benchmarks/lensing.py"
+    # Run benchmark via slurm
+    HEALPY_DATAPATH="$HEALPY_DATAPATH" ARRAY_BACKEND="$ARRAY_BACKEND" srun "$GLASS_DIR/.venv/bin/python" "$GLASS_DIR/benchmarks/lensing.py"
 done
